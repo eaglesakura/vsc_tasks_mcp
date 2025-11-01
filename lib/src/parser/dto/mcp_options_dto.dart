4a04c9878dbd4db68392ee1db4ad76a2f1ecedaa
eaglesakura <eagle.sakura@gmail.com>
2025-11-01 18:37:02 +0900

    add: Github Actions設定を追加

--- lib/src/parser/dto/mcp_options_dto.dart
+++ lib/src/parser/dto/mcp_options_dto.dart
@@ -8,7 +8,6 @@ part "mcp_options_dto.g.dart";
 /// options.mcpで定義されているMCPツール設定のDTO
 @freezed
 abstract class McpOptionsDto with _$McpOptionsDto {
-
   const factory McpOptionsDto({
     @JsonKey(name: "tool") required String tool,
     @JsonKey(name: "description") String? description,
